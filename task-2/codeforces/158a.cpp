6a98664f4a6d79babc1c024edc26a54984306c5e
Ashwin Ramakrishnan <ashwinkey04@gmail.com>
2019-07-29 21:52:58 +0530

    Update 158a.cpp

--- task-2/codeforces/158a.cpp
+++ task-2/codeforces/158a.cpp
@@ -2,23 +2,21 @@
 
 using namespace std;
 
-int main()
-{
+int main(){
     int n,k;
     cin >>n>>k;
     int a;
-    for(int i=0;i<k;++i)
-    {
+    for(int i=0;i<k;++i){
         cin>>a;
-        if (a==0)
-        {   cout<<i;
+        if (a==0){
+            cout<<i;
             exit(0);
         }
     }
 
     int b;
-    for(int i=k;i<n;++i)
-    {   cin>>b;
+    for(int i=k;i<n;++i){
+        cin>>b;
         if (b != a)
         {
             cout << i << endl;
