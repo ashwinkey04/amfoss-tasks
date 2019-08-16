a49a798446f97c48bd77db76f70d4c7ccaac717b
Ashwin Ramakrishnan <ashwinkey04@gmail.com>
2019-08-16 18:12:03 +0530

    Added final source code (works with input)

---
+++ task-7/src/main.rs
@@ -0,0 +1,14 @@
+extern crate regex;
+use std::io;
+use regex::Regex;
+
+fn main() {
+    let re = Regex::new(r"\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$").unwrap();    
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)
+        .ok()
+        .expect("Input error");
+    println!("{:?}",&input);
+    println!("{:?}",len);    
+    println!("Match? {}", re.is_match(&input.trim()));
+}
