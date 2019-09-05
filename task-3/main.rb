364960e70f06929fa3bffc96a84f1b1395fb35cb
Ashwin R <ashwinkey04@gmail.com>
2019-09-05 19:10:38 +0530

    Final working code

--- task-3/main.rb
+++ task-3/main.rb
@@ -1,31 +1,13 @@
 
 require 'nokogiri'
-require 'open-uri'  
-
-# Fetch and parse HTML document
-name = gets
-name = name.chomp
-puts name
-
-doc = Nokogiri::HTML(open('https://www.google.com/search?q=Linux'))
-doc.css('div.ellip')collect do |qry|
-    qry.css('h2').text
+require 'open-uri'
+require 'pry'
 
 
-#doc = Nokogiri::HTML(open('https://nokogiri.org/tutorials/installing_nokogiri.html'))
+puts("Enter search query: ")
+userin = gets
+doc = Nokogiri::HTML(open('https://www.google.com/search?start=0&end=11&q='+userin))
+doc.xpath('//div/a/div[text()]').each do |link|
+    puts link.content
+end
 
-#puts "---------------------------------------------"
-#puts "### Search for nodes by css"
-#doc.css('nav ul.menu li a', 'article h2').each do |link|
-#  puts link.content
-#end
-#puts "--------------------------------------------"
-#puts "### Search for nodes by xpath"
-#doc.xpath('//nav//ul//li/a', '//article//h2').each do |link|
- # puts link.content
-#end
-#puts " ------------------------------------------- "
-#puts "### Or mix and match."
-#doc.search('nav ul.menu li a', '//article//h2').each do |link|
- # puts link.content
-#end
--- task-3/medium.rb
+++
@@ -1,9 +0,0 @@
-require 'nokogiri'
-require 'pry'
-require 'open-uri'
-
-doc = Nokogiri::HTML(open('https://www.yogajournal.com/poses/types'))
-
-doc.css('.m-card--header').collect do |pose_type|
-    pose_type.css('h2').text
-end 
